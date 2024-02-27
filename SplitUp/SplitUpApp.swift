d8a8559283baacbeffb7ade1149ff4cf4bb7350d
Naveen Boopathi <naveenboopathi18@outlook.com>
2024-02-27 09:54:48 +0530

    Initial Commit

---
+++ SplitUp/SplitUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplitUpApp.swift
+//  SplitUp
+//
+//  Created by Naveen Boopathi on 27/02/24.
+//
+
+import SwiftUI
+
+@main
+struct SplitUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
